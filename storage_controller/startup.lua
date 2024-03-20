8c5b7263bf3193ee4d3b89c82e649d713e2bb42a
Portablefire22 <barnowlsarecool@gmail.com>
2024-03-20 23:50:37 +0000

    Added a simple test init

--- storage_controller/startup.lua
+++ storage_controller/startup.lua
@@ -4,7 +4,16 @@ Buffer_chest = peripheral.find("sophisticatedstorage:chest") -- Holds items for 
 Buffer_barrel = peripheral.find("minecraft:barrel") -- To move items to and from the vault
 Monitor = peripheral.find("monitor")
 
+function Determine_state() -- Figure out what state the storage system was left in
+  local file = fs.open("storage_state", "r")
+  if file == nil then 
+    Monitor.clear()
+    Monitor.write("Previous storage state was not saved!\nAssuming system is in perfect condition!")
+    return
+  end 
+end
+
 function Start()
   Monitor.setTextColour(colours.blue)
-  Monitor.write("Initialising Storage..")
+  Monitor.write("Initialising Storage...")
 end
