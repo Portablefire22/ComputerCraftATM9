3e6e94ec8b804b0ed495a14cdeda50a32af1aa95
Portablefire22 <barnowlsarecool@gmail.com>
2024-03-21 00:26:04 +0000

    Updated formatting

--- storage_controller/startup.lua
+++ storage_controller/startup.lua
@@ -5,17 +5,20 @@ Buffer_barrel = peripheral.find("minecraft:barrel") -- To move items to and from
 Monitor = peripheral.find("monitor")
 
 function Determine_state() -- Figure out what state the storage system was left in
-  local file = fs.open("storage_state", "r")
+  local file = fs.open("storage_state.dat", "r")
   if file == nil then
     Monitor.clear()
     Monitor.setTextColour(colours.red)
-    Monitor.setCursorPos(0,0)
-    Monitor.write("Previous storage state was not saved!\nAssuming system is in perfect condition!")
+    Monitor.setCursorPos(1,1)
+    Monitor.write("Previous storage state was not saved!")
+    Monitor.setCursorPos(1,2)
+    Monitor.write("Assuming system is in perfect condition!")
     return
   end
 end
 
 function Start()
+  Monitor.setCursorPos(1,1)
   Monitor.setTextColour(colours.white)
   Monitor.write("Initialising Storage...")
   Determine_state()
