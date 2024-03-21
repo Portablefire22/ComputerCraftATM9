c9ddc47c7fa88ca314428972cbad74e6e15ea0ab
Portablefire22 <barnowlsarecool@gmail.com>
2024-03-21 05:05:12 +0000

    Debug message

--- storage_controller/startup.lua
+++ storage_controller/startup.lua
@@ -182,6 +182,7 @@ function Get_time_to_move(blocks)
   local rpm = 256
   local tick_in_second = 20
   local speed_to_rpm = 512
+  Chat_box.sendMessage(string.format("Time: %f", math.abs(blocks) * speed_to_rpm / (rpm * tick_in_second)))
   return math.abs(blocks) * speed_to_rpm / (rpm * tick_in_second)
 end
 
