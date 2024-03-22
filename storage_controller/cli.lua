cda9cd07fdf1e831e91cbc1e0e7531d79f1a1ad0
Portablefire22 <barnowlsarecool@gmail.com>
2024-03-22 20:59:14 +0000

    Trying to highlight a single letter

--- storage_controller/cli.lua
+++ storage_controller/cli.lua
@@ -41,18 +41,37 @@ function M.process_input(raw_key)
     key = "9"
   elseif key == "rightShift" or key == "leftShift" then 
     M.shift = true
+    key = ""
   elseif key == "capsLock" then
     M.caps = not M.caps
+    key = ""
+  elseif key == "enter" then
+    key = "\n"
   end
   if key == "backspace" then
     M.input_buffer = M.input_buffer:sub(1, -2)
   else
     if M.caps or M.shift then 
-      key = key.upper()
+      key = key:upper()
     end
     M.input_buffer = M.input_buffer .. key
   end
-  print(M.input_buffer)
+  print(tryWrite(M.input_buffer, " > ", colour.orange))
+end
+
+local function tryWrite( sLine, regex, colour )
+  local match = string.match( sLine, regex )
+  if match then
+        if type(colour) == "number" then
+          term.setTextColour( colour )
+        else
+          term.setTextColour( colour(match) )
+        end
+        term.write( match )
+        term.setTextColour( textColour )
+        return string.sub( sLine, string.len(match) + 1 )
+  end
+  return nil
 end
 
 return M
