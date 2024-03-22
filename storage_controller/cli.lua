f6da22ac53063a797a37842f72e3f7d9bb9f948b
Portablefire22 <barnowlsarecool@gmail.com>
2024-03-22 19:39:38 +0000

    Added some more characters to the CLI input

--- storage_controller/cli.lua
+++ storage_controller/cli.lua
@@ -8,10 +8,34 @@ function M.process_input(raw_key)
   local key = keys.getName(raw_key)
   if key == "space" then
     key = " "
-  else if key == "slash" then
+  elseif key == "slash" then
     key = "/"
+  elseif key == "zero" then
+    key = "0"
+  elseif key == "one" then
+    key = "1"
+  elseif key == "two" then
+    key = "2"
+  elseif key == "three" then
+    key = "3"
+  elseif key == "four" then
+    key = "4"
+  elseif key == "five" then
+    key = "5"
+  elseif key == "six" then
+    key = "6"
+  elseif key == "seven" then
+    key = "7"
+  elseif key == "eight" then
+    key = "8"
+  elseif key == "nine" then
+    key = "9"
   end
-  M.input_buffer = M.input_buffer .. key
+  if key == "backspace" then
+    M.input_buffer = M.input_buffer:sub(1, -2)
+  else
+    M.input_buffer = M.input_buffer .. key
+  end
   print(M.input_buffer)
 end
 
